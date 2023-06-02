ab75ebeec4d146160b20347253506666fa0e5211
Christoph Reiter <reiter.christoph@gmail.com>
2023-06-02 18:02:41 +0200

    wip

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+pacman --noconfirm -U "https://github.com/lazka/python-crash-test/releases/download/temp/mingw-w64-ucrt-x86_64-python-3.9.13-2-any.pkg.tar.zst"
+
 git clone --depth=1 https://github.com/mesonbuild/meson.git meson || true
 cd project
 rm -Rf _build
