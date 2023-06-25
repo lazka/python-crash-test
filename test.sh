e8cc8d093feb8dbf807d6f2d8b916e88c575c032
Christoph Reiter <reiter.christoph@gmail.com>
2023-06-25 09:38:53 +0200

    wip

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ set -e
 git clone --depth=1 https://github.com/mesonbuild/meson.git meson || true
 cd project
 rm -Rf _build
-python ../meson/meson.py setup _build
+python3.11 ../meson/meson.py setup _build
 cd _build
 ninja
